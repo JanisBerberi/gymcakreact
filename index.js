335db69cfe7e7bd9b1b99f20cc821ba2859d0ff4
JanisBerberi <janisberberi@gmail.com>
2021-04-26 12:59:25 +0000

    test odd number

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Ahoj Gymčaku.")
+const isNotOdd = require('is-not-odd');
+ 
+console.log(isNotOdd('0')); //=> true
+console.log(isNotOdd('2')); //=> true
+ 
+console.log(isNotOdd(1)); //=> false
+console.log(isNotOdd(3)); //=> false
